506c5499f37b818482e33a1879e91d019622e1cd
mohsin-wahdi <amman4781@gmail.com>
2024-12-01 12:42:41 +0530

    changes

--- app.js
+++ app.js
@@ -10,16 +10,43 @@
 //     res.end("This is Contact Page");
 //   }
 // });
-
 // server.listen(3000);
 
+// writing the code in express js
+// const express = require("express");
+// const app = express();
+// app.get("/", (req, res) => {
+//   res.send("hello world aman");
+// });
+// app.get("/about", (req, res) => {
+//   res.send("About");
+// });
+
+// params url me change karne par server me change ho
+// app.get("/profile/:username", (req, res) => {
+//   res.send(`Hello From ${req.params.username}`);
+// });
+// app.listen(3000);
+
+// starting ejs
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world aman~");
+// declare the ejs
+app.set("view engine", "ejs");
+
+// declare the static files in ejs
+app.use(express.static("./public"));
+
+// routing the express
+app.get("/", function (req, res) {
+  res.render("index");
 });
-app.get("/about", (req, res) => {
-  res.send("About");
+app.get("/contact", function (req, res) {
+  res.render("contact");
 });
+
+app.use()
+
+
 app.listen(3000);
